95fb4b072015684b1cc6c240981dfc211aec1051
Nischal Dhoj Kunwar <121807367+nischalk17@users.noreply.github.com>
2024-12-21 18:59:20 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// script.js
-
 document.addEventListener("DOMContentLoaded", function () {
     const sidebar = document.getElementById("sidebar");
     const sidebarToggle = document.getElementById("sidebar-toggle");
